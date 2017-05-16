14a82cd93ff27339693a657a2e5fd900d617bed5
Charlie Wood <charliemowood@gmail.com>
2017-05-17 00:13:25 +0100

    Set up Photographer class and test plus hasName()

---
+++ Photographer.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Photographer {
+
+  private String name;
+
+  public Photographer(String name){
+    this.name = name;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+}
---
+++ PhotographerTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+import static org.junit.Assert.assertNotNull;
+
+public class PhotographerTest{
+  Photographer photographer;
+
+  @Before
+  public void before() {
+    photographer = new Photographer("Steve");
+  }
+
+  @Test
+  public void hasName(){
+    assertEquals("Steve", photographer.getName());
+  }
+}
